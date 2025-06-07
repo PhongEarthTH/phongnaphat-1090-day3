a33835fcec53addc3d5f537ceacdeb650e050ee4
phongnaphat <phongnaphat.ch@mail.wu.ac.th>
2025-06-07 14:04:33 +0700

    feat: implement login and home pages with routing
    - Added a login page with form validation and submission handling.
    - Created a home page with a simple message.
    - Configured routing to include the login page.
    - Integrated Tailwind CSS for styling.
    - Added PostCSS configuration for Tailwind.
    - Created unit tests for both login and home components.

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted:', this.loginForm.value);
+    } else {
+      console.log('Form Invalid');
+    }
+  }
+}
